b2e9ce1b6066b841a2a6687c614eeaa7c4537168
mojeedkusimo <mkusimo90@gmail.com>
2020-11-06 08:06:25 +0100

    Frontend Professional Challenge in progress...

--- frontend-professional/script.js
+++ frontend-professional/script.js
@@ -2,10 +2,6 @@
 let projectImg = document.querySelectorAll(".project-img");
 let projectPara = document.querySelectorAll(".project-p");
 let projectSpan = document.querySelectorAll(".project-span");
-let socialMediaLinks = document.querySelectorAll("li.social-media-links");
-let moreProjects = document.querySelector("#more");
-let showAll = document.querySelector("#show-all a");
-
 
 for (let i = 0; i < projectImg.length; i++) {
     projectImg[i].addEventListener("mouseover", function() {
@@ -30,14 +26,3 @@ for (let i = 0; i < projectImg.length; i++) {
         projectSpan[i].style.visibility = "hidden";
     });
 }
-
-showAll.addEventListener("click", function(e) {
-    e.preventDefault();
-    if (showAll.textContent === "Show more") {
-        moreProjects.style.display = "flex";
-        showAll.textContent = "Hide Projects";
-    } else {
-        moreProjects.style.display = "none";
-        showAll.textContent = "Show more";
-    }
-});
